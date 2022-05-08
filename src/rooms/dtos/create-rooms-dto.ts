7c64b28d0884e8c6095fefe7611e12b773df5de0
Alexander <impoossible.777@gmail.com>
2022-05-08 22:28:06 +0400

    added dto for creation  room

--- src/rooms/dtos/create-rooms-dto.ts
+++ src/rooms/dtos/create-rooms-dto.ts
@@ -1,57 +1,100 @@
-import { IsNotEmpty, IsString, Validate, ValidateIf } from "class-validator";
+import { Type } from "class-transformer";
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsNumberString,
+  IsString,
+  Validate,
+  ValidateIf
+} from "class-validator";
 
 
 export class CreateRoomsDto {
   @IsNotEmpty()
+  @Type(() => Number)
+  @IsInt()
   roomNumber: number;
 
   @IsNotEmpty()
+  @Type(() => Number)
+  @IsInt()
   price: number;
 
   @IsNotEmpty()
-  numberBedrooms: number;
+  @Type(() => Number)
+  @IsInt()
+  countBedrooms: number;
 
   @IsNotEmpty()
-  numberBeds: number;
+  @Type(() => Number)
+  @IsInt()
+  countBeds: number;
 
   @IsNotEmpty()
-  numberBathrooms: number;
+  @Type(() => Number)
+  @IsInt()
+  countBathrooms: number;
 
-  // @IsNotEmpty()
-  // rating: number;
   @IsNotEmpty()
+  @Type(() => Number)
+  @IsInt()
   corridorWidth: number;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   desktop: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   chairForFeeding: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   crib: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   airConditioning: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   noiseAbsorbingWalls: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   windowInEveryBedroom: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   smoke: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   pets: boolean;
 
   @IsNotEmpty()
+  @Type(() => Boolean)
+  @IsBoolean()
   guests: boolean;
 
-
+  @IsString()
   @ValidateIf((o) => o.imgRooms || o.imgRooms === '')
-  imgRooms: string
+  imgsRoom: string
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  rating: number
 }
 
 // @IsString()
